8be61fe778dd3c79a95c480593a80de5f7014007
Ansh <tayalansh176@gmail.com>
2025-11-21 20:48:34 +0530

    updates

---
+++ weird_algorithm.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long n;
+    cin>>n;
+    while(n!=1){
+        cout<<n<<" ";
+        if(n%2==0){
+            n=n/2;
+        }
+        else{
+            n=3*n+1;
+        }
+    }
+    cout<<1<<endl;
+    return 0;
+}
